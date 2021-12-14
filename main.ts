458b4fc62b8d1ceebb1fd399813ee3e0c6c2d2a2
AnsonShie <anson12405@gmail.com>
2021-12-14 21:26:50 +0800

    CHG waiting time and add last check when number is zero

--- main.ts
+++ main.ts
@@ -3,34 +3,39 @@ let target = 0
 input.onButtonPressed(Button.A, function () {
     count = 0
     target = 20
-    basic.pause(100)
+    basic.pause(1000)
     basic.showNumber(4)
     if (count >= target) {
         basic.showIcon(IconNames.Heart)
         music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
     } else {
-        basic.pause(100)
+        basic.pause(1000)
         basic.showNumber(3)
         if (count >= target) {
             basic.showIcon(IconNames.Heart)
             music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
         } else {
-            basic.pause(100)
+            basic.pause(1000)
             basic.showNumber(2)
             if (count >= target) {
                 basic.showIcon(IconNames.Heart)
                 music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
             } else {
-                basic.pause(100)
+                basic.pause(1000)
                 basic.showNumber(1)
                 if (count >= target) {
                     basic.showIcon(IconNames.Heart)
                     music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
                 } else {
-                    basic.pause(100)
+                    basic.pause(1000)
                     basic.showNumber(0)
                     basic.showIcon(IconNames.No)
-                    music.startMelody(music.builtInMelody(Melodies.Punchline), MelodyOptions.Once)
+                    if (count >= target) {
+                        basic.showIcon(IconNames.Heart)
+                        music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
+                    } else {
+                        music.startMelody(music.builtInMelody(Melodies.Punchline), MelodyOptions.Once)
+                    }
                 }
             }
         }
