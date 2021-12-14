a405c7206e5ad2501e4f14ce64e0e39c28d4f1e1
AnsonShie <anson12405@gmail.com>
2021-12-14 21:39:07 +0800

    CHG the target number

--- main.ts
+++ main.ts
@@ -2,38 +2,37 @@ let count = 0
 let target = 0
 input.onButtonPressed(Button.A, function () {
     count = 0
-    target = 20
-    basic.pause(1000)
+    target = 8
     basic.showNumber(4)
     if (count >= target) {
         basic.showIcon(IconNames.Heart)
         music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
     } else {
-        basic.pause(1000)
+        basic.pause(2000)
         basic.showNumber(3)
         if (count >= target) {
             basic.showIcon(IconNames.Heart)
             music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
         } else {
-            basic.pause(1000)
+            basic.pause(2000)
             basic.showNumber(2)
             if (count >= target) {
                 basic.showIcon(IconNames.Heart)
                 music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
             } else {
-                basic.pause(1000)
+                basic.pause(2000)
                 basic.showNumber(1)
                 if (count >= target) {
                     basic.showIcon(IconNames.Heart)
                     music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
                 } else {
-                    basic.pause(1000)
+                    basic.pause(2000)
                     basic.showNumber(0)
-                    basic.showIcon(IconNames.No)
                     if (count >= target) {
                         basic.showIcon(IconNames.Heart)
                         music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
                     } else {
+                        basic.showIcon(IconNames.No)
                         music.startMelody(music.builtInMelody(Melodies.Punchline), MelodyOptions.Once)
                     }
                 }
